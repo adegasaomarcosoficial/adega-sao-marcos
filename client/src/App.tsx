9ecaf42abba5baf6c93b7868bef14368ba990c94
adegasaomarcosoficial <markosouz@gmail.com>
2026-05-30 17:34:35 -0300

    feat: add admin route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,13 +6,14 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import AgeVerification from "./components/AgeVerification";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/admin"} component={Admin} />
       <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
       <Route component={NotFound} />
     </Switch>
   );
